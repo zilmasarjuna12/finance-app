55acc91e72f12ba22c8852b4ff18c787f37692da
zilmas <zilmas@smartm2m.co.kr>
2025-09-13 22:49:27 +0700

    adding permission check

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,8 +1,18 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+import { useAuthStore } from "@/features/auth/stores/auth";
 
 import { LoginContainer } from "@/features/auth";
 
 export const Route = createFileRoute("/login")({
+  beforeLoad: async () => {
+    const isAuthenticated = useAuthStore.getState().isAuthenticated;
+    if (isAuthenticated) {
+      throw redirect({
+        to: "/main/transactions",
+      });
+    }
+  },
   component: RouteComponent,
 });
 
--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -1,8 +1,18 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+import { useAuthStore } from "@/features/auth/stores/auth";
 
 import { RegisterContainer } from "@/features/auth";
 
 export const Route = createFileRoute("/register")({
+  beforeLoad: async () => {
+    const isAuthenticated = useAuthStore.getState().isAuthenticated;
+    if (isAuthenticated) {
+      throw redirect({
+        to: "/main/transactions",
+      });
+    }
+  },
   component: RouteComponent,
 });
 
